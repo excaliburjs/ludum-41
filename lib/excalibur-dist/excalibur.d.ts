7336ad5d08b9f5fc1971964274f806fbd572f874
jedeen <jedeen@users.noreply.github.com>
2018-04-15 18:23:44 -0500

    Include excalibur dist, make additional changes

---
+++ lib/excalibur-dist/excalibur.d.ts
@@ -0,0 +1,9 @@
+/*! excalibur - vlocal - 2018-04-15
+* https://github.com/excaliburjs/Excalibur
+* Copyright (c) 2018 Excalibur.js <https://github.com/excaliburjs/Excalibur/graphs/contributors>; Licensed BSD-2-Clause
+* @preserve */
+/// <reference path="excalibur.amd.d.ts" />
+
+// Expose all public API as ex namespace
+export * from 'Index';
+export as namespace ex;
