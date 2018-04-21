d3944eb6fbf889b6fa82ced3164883b41792fdcd
Kamran Ayub <kamran.ayub@gmail.com>
2018-04-21 16:28:54 -0500

    Background drawing

--- game/config.ts
+++ game/config.ts
@@ -7,7 +7,7 @@ export default {
     "https://ludum41stats.azurewebsites.net/api/HttpLudum41StatsTrigger?code=eumYNdyRh0yfBAk0NLrfrKkXxtGsX7/Jo5gAcYo13k3GcVFNBdG3yw==",
 
   GameWidth: 800, // pixels
-  GameHeight: 600, // pixels
+  GameHeight: 800, // pixels
 
   // Top Floor config
   Floor: {
--- game/resources.ts
+++ game/resources.ts
@@ -4,6 +4,7 @@ export default {
   sampleImg: new Texture("game/assets/img/sample-image.png"),
   txBike: new Texture("game/assets/img/bike.png"),
   txCrate: new Texture("game/assets/img/crate.png"),
+  txBackground: new Texture("game/assets/img/top-bg.png"),
 
   sampleSnd: new Sound("game/assets/snd/sample-sound.wav")
 };
