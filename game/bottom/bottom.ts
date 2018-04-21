e6e6db454876fe7777524c00a75e62aec7f8a010
Alan Grgic <alan.grgic@genmills.com>
2018-04-21 14:24:16 -0500

    add visuals for bottom game

--- game/bottom/bottom.ts
+++ game/bottom/bottom.ts
@@ -1,11 +1,16 @@
 import * as ex from "excalibur";
+import { CollatingGame } from "./collating-game/collatingGame";
 
 export class BottomSubscene {
   constructor() {}
-  public setup(scene: ex.Scene) {}
+  public setup(scene: ex.Scene) {
+    this.startPaperCollating(scene);
+  }
 
-  public startPaperCollating() {
+  public startPaperCollating(scene) {
     // TODO load the paper collating mini-game
+    var collatingGame = new CollatingGame(scene);
+    collatingGame.show();
   }
 
   public startTalkToCoworker() {
