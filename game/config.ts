a14486d0336e5a7c3f88fe6a5d485e69ee4677cb
Erik Onarheim <erik.onarheim@gmail.com>
2018-04-21 09:50:44 -0500

    Create top and floor stubs

--- game/config.ts
+++ game/config.ts
@@ -1,4 +1,8 @@
 var Config = {
+  // Analytics config
   AnalyticsEndpoint:
-    "https://ludum41stats.azurewebsites.net/api/HttpLudum41StatsTrigger?code=eumYNdyRh0yfBAk0NLrfrKkXxtGsX7/Jo5gAcYo13k3GcVFNBdG3yw=="
+    "https://ludum41stats.azurewebsites.net/api/HttpLudum41StatsTrigger?code=eumYNdyRh0yfBAk0NLrfrKkXxtGsX7/Jo5gAcYo13k3GcVFNBdG3yw==",
+
+  // Floor config
+  FloorSpeed: -100
 };
