4f424074541cfd6c1282f5121e37dba39a0349ae
Travis-CI <travis@excaliburjs.com>
2018-04-21 14:40:22 +0000

    Deploying game for b5b7682ff9b16880cd2bfb628aff5554df5d174c

--- lib/excalibur-dist/excalibur.d.ts
+++ lib/excalibur-dist/excalibur.d.ts
@@ -1,9 +1,9 @@
-/*! excalibur - vlocal - 2018-04-17
+/*! excalibur - vv0.16.0-7dd4812 - 2018-04-21
 * https://github.com/excaliburjs/Excalibur
 * Copyright (c) 2018 Excalibur.js <https://github.com/excaliburjs/Excalibur/graphs/contributors>; Licensed BSD-2-Clause
 * @preserve */
 /// <reference path="excalibur.amd.d.ts" />
 
 // Expose all public API as ex namespace
-export * from 'Index';
+export * from "Index";
 export as namespace ex;
