1f3106aaaec9db63e20712f5b503e2e3af70066b
Erik Onarheim <erik.onarheim@gmail.com>
2018-04-21 12:24:45 -0500

    Add health system

---
+++ game/top/health.ts
@@ -0,0 +1,21 @@
+import * as ex from "excalibur";
+import Config from "../config";
+
+export class TopHealth extends ex.Label {
+  public health: number = Config.Health.Default;
+  constructor(engine: ex.Engine) {
+    super({
+      x: Config.Health.Pos.x,
+      y: Config.Health.Pos.y + Config.Health.FontSize,
+      fontSize: Config.Health.FontSize,
+      fontUnit: ex.FontUnit.Px,
+      anchor: ex.Vector.Zero.clone(),
+      color: ex.Color.Red,
+      text: Config.Health.Default.toString()
+    });
+  }
+
+  onPostUpdate(engine: ex.Engine, delta: number) {
+    this.text = this.health.toString();
+  }
+}
--- game/top/top.ts
+++ game/top/top.ts
@@ -1,17 +1,21 @@
 import * as ex from "excalibur";
 import { Floor } from "./floor";
 import { TopPlayer } from "./top-player";
+import { TopHealth } from "./health";
 
 export class Top {
   floor: Floor;
   player: TopPlayer;
+  health: TopHealth;
   constructor(private _engine: ex.Engine) {
     this.floor = new Floor(_engine);
     this.player = new TopPlayer(_engine);
+    this.health = new TopHealth(_engine);
   }
 
   public setup(scene: ex.Scene) {
     scene.add(this.floor);
     scene.add(this.player);
+    scene.add(this.health);
   }
 }
