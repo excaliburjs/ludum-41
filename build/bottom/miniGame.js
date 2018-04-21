ebc3b7c1faec2dae227a25cfa1313e7dab920604
Travis-CI <travis@excaliburjs.com>
2018-04-21 17:42:24 +0000

    Deploying game for e0e9781dd42034e477fb498b98f50a7e57755d83

--- build/bottom/miniGame.js
+++ build/bottom/miniGame.js
@@ -1,6 +1,19 @@
-class MiniGame {
-    show() { }
-    hide() { }
+export class MiniGame {
+    show() {
+        if (!this._isSetUp) {
+            this.setup(); //initialize actors and add them to the scene and miniGameActors collection.
+        }
+        this._isSetUp = true;
+        this.reset();
+        for (let i = 0; i < this.miniGameActors.length; i++) {
+            this.miniGameActors[i].visible = true;
+        }
+    }
+    hide() {
+        for (let i = 0; i < this.miniGameActors.length; i++) {
+            this.miniGameActors[i].visible = false;
+        }
+    }
     onSucceed() {
         this.hide();
     }
