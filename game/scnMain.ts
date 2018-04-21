318af2310675d3f9954968d3c27449cfcbea167d
Erik Onarheim <erik.onarheim@gmail.com>
2018-04-21 12:43:07 -0500

    Tick down health, rename to TopSubscene and BottomSubscene

--- game/scnMain.ts
+++ game/scnMain.ts
@@ -1,12 +1,12 @@
 import * as ex from "excalibur";
-import { Top } from "./top/top";
-import { Bottom } from "./bottom/bottom";
+import { TopSubscene } from "./top/top";
+import { BottomSubscene } from "./bottom/bottom";
 
 export class ScnMain extends ex.Scene {
   constructor(engine: ex.Engine) {
     super(engine);
-    let top = new Top(engine);
-    let bottom = new Bottom();
+    let top = new TopSubscene(engine);
+    let bottom = new BottomSubscene();
 
     top.setup(this);
     bottom.setup(this);
