cbbad489254fdf3f72c8c82ae8d045253827338e
Travis-CI <travis@excaliburjs.com>
2018-04-21 16:13:04 +0000

    Deploying game for 938ff847523b93bd2c126adfc6264d112815b6ca

---
+++ build/document.js
@@ -0,0 +1,46 @@
+import { Actor } from "excalibur";
+class OfficeDoc extends Actor {
+    constructor(pageNumber) {
+        super();
+        this._pageNumber = pageNumber;
+    }
+    get pageNumber() {
+        return this._pageNumber;
+    }
+}
+class OfficeDocSet {
+    constructor(numDocuments) {
+        this._documents = [];
+        this._playerSortedStack = [];
+        this._numDocuments = numDocuments;
+        for (var i = 0; i < this._numDocuments; i++) {
+            this._documents.push(new OfficeDoc(i));
+        }
+    }
+    //attempt to add this document to the sorted stack
+    tryAddToSortedStack(documentIn) {
+        if (documentIn.pageNumber === this._playerSortedStack.length) {
+            this._playerSortedStack.push(documentIn);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    isComplete() {
+        return this._playerSortedStack.length === this._numDocuments;
+    }
+    getScrambledDocumentSet() {
+        var docsArr = this._documents;
+        var currentIndex = docsArr.length, temporaryValue, randomIndex;
+        while (0 !== currentIndex) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            temporaryValue = docsArr[currentIndex];
+            docsArr[currentIndex] = docsArr[randomIndex];
+            docsArr[randomIndex] = temporaryValue;
+        }
+        return docsArr;
+    }
+}
+//# sourceMappingURL=document.js.map
