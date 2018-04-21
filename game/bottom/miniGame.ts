e0e9781dd42034e477fb498b98f50a7e57755d83
Alan Grgic <alan.grgic@genmills.com>
2018-04-21 12:40:30 -0500

    add miniGame base logic

--- game/bottom/miniGame.ts
+++ game/bottom/miniGame.ts
@@ -1,9 +1,27 @@
-abstract class MiniGame {
+import { Actor } from "excalibur";
+export abstract class MiniGame {
+  protected miniGameActors: Array<Actor>;
+  private _isSetUp: boolean;
+
   protected abstract setup(): void;
+  protected abstract reset(): void;
 
-  public show(): void {}
+  public show(): void {
+    if (!this._isSetUp) {
+      this.setup(); //initialize actors and add them to the scene and miniGameActors collection.
+    }
+    this._isSetUp = true;
+    this.reset();
+    for (let i = 0; i < this.miniGameActors.length; i++) {
+      this.miniGameActors[i].visible = true;
+    }
+  }
 
-  public hide(): void {}
+  public hide(): void {
+    for (let i = 0; i < this.miniGameActors.length; i++) {
+      this.miniGameActors[i].visible = false;
+    }
+  }
 
   public onSucceed(): void {
     this.hide();
