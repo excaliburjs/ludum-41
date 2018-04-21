70371c92ce8fb142add2ecb7c16c0a1cf77f473a
Travis-CI <travis@excaliburjs.com>
2018-04-21 21:56:11 +0000

    Deploying game for 1b7fbd836349764197bb951051ff03b98dd14d9c

--- build/top/background.js
+++ build/top/background.js
@@ -16,6 +16,10 @@ export class Background extends ex.Actor {
         });
         this.drawSlices = new Array(Background.Slices * 2);
         this.slices = new Array(Background.Slices);
+        this._offscreen = document.createElement("canvas");
+        this._offscreen.width = Background.Slices * Background.SliceWidth * 2;
+        this._offscreen.height = engine.drawHeight;
+        this._offscreenCtx = this._offscreen.getContext("2d");
     }
     onInitialize() {
         const { txBackground } = Resources;
@@ -30,27 +34,33 @@ export class Background extends ex.Actor {
             });
         }
         this.fillSlices(0, Background.Slices * 2);
+        this.drawToOffscreen(0, Background.Slices * 2);
     }
     onPostUpdate(engine, delta) {
         if (this.x <= -engine.halfDrawWidth) {
             ex.Logger.getInstance().info("Reset background");
             this.x = 0;
             this.drawSlices = this.drawSlices.slice(16);
+            this.drawToOffscreen(0, 16);
             this.fillSlices(16, Background.Slices * 2);
         }
     }
     onPostDraw(ctx, delta) {
-        for (let i = 0; i < this.drawSlices.length; i++) {
-            const slice = this.drawSlices[i];
-            const x = Math.floor(this.x + i * Background.SliceWidth);
-            slice && slice.draw(ctx, x, this.y);
-        }
+        ctx.drawImage(this._offscreen, this.x, this.y);
     }
     fillSlices(start, to) {
         for (let i = start; i < to; i++) {
             this.drawSlices[i] = this.slices[Config.Rand.integer(0, this.slices.length - 1)];
         }
+        this.drawToOffscreen(start, to);
         ex.Logger.getInstance().info("Picked bg slices", this.drawSlices.length);
+    }
+    drawToOffscreen(start, to) {
+        for (let i = start; i < to; i++) {
+            const slice = this.drawSlices[i];
+            const x = i * Background.SliceWidth;
+            slice && slice.draw(this._offscreenCtx, x, 0);
+        }
     }
 }
 Background.Slices = 16;
