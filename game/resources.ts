0ce2be0b70b4c5b73c3bdf8642090777b6a0ee78
jedeen <jedeen@users.noreply.github.com>
2018-04-22 09:19:39 -0500

    Remove sample image

--- game/resources.ts
+++ game/resources.ts
@@ -1,7 +1,6 @@
 import { Texture, Sound } from "excalibur";
 
 export default {
-  sampleImg: new Texture("game/assets/img/sample-image.png"),
   txBike: new Texture("game/assets/img/bike.png"),
   txCrate: new Texture("game/assets/img/crate.png"),
   txBackground: new Texture("game/assets/img/top-bg.png"),
