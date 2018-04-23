59a57536f4b4b47e1e64403f29cc6682f8321e98
Erik Onarheim <erik.onarheim@gmail.com>
2018-04-23 14:13:38 -0500

    Hearts!

--- game/resources.ts
+++ game/resources.ts
@@ -6,6 +6,7 @@ export default {
   txBombSpriteSheet: new Texture("game/assets/img/bomb.png"),
   txBackground: new Texture("game/assets/img/top-bg.png"),
   txGurter: new Texture("game/assets/img/gurter.png"),
+  txHeartSpriteSheet: new Texture("game/assets/img/heart.png"),
 
   txCollateBackground: new Texture("game/assets/img/collate-bg.png"),
   txDocPieChart: new Texture("game/assets/img/pieChartSubmit.png"),
